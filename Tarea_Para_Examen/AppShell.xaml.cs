0fca671830fec14345ee816c1e8772e4a65932b2
Moises Chavez <gchavez19956@gmail.com>
2023-11-08 22:07:58 -0500

    Add project files.

---
+++ Tarea_Para_Examen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarea_Para_Examen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tarea_Para_Examen/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tarea_Para_Examen
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
